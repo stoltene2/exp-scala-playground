990238f31156a4ef2ae46646a2291c5672066d7c
Eric Stolten <eric.stolten@adp.com>
2016-06-07 18:09:46 -0400

    Implement functional isSorted

--- src/main/scala/com/functional/Ch2.scala
+++ src/main/scala/com/functional/Ch2.scala
@@ -9,4 +9,9 @@ object Ch2 {
 
     go(n, 1, 1)
   }
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    if (as.length < 2) true
+    else as.zip(as.drop(1)).forall( x => ordered(x._1, x._2))
+  }
 }
