97354ece5c5e609eb2c8ebacb2b168a25ddf22c1
Eric Stolten <eric.stolten@adp.com>
2016-06-08 08:35:58 -0400

    Add curry, uncurry, and compose

--- src/main/scala/com/functional/Ch2.scala
+++ src/main/scala/com/functional/Ch2.scala
@@ -14,4 +14,18 @@ object Ch2 {
     if (as.length < 2) true
     else as.zip(as.drop(1)).forall( x => ordered(x._1, x._2))
   }
+
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = {
+    (a: A) => {
+      (b: B) => f(a,b)
+    }
+  }
+
+  def uncurry[A,B,C](f: A => (B => C)): (A, B) => C = {
+    (a: A, b: B) => f(a)(b)
+  }
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    (x: A) => f(g(x))
+  }
 }
